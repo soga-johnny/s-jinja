3e83bc6fa1591cc8d771bab89d2cc084d7243d21
soga-johnny <soga.johnny@gmail.com>
2023-07-17 20:57:22 +0900

    addHeader

--- src/app/component.js
+++ src/app/component.js
@@ -1,50 +1,50 @@
-"use client"
+"use client";
 
-import Image from 'next/image'
-import styles from './page.module.scss'
-import Spline from '@splinetool/react-spline'
-import Link from 'next/link'
+// import Image from "next/image";
+import styles from "./page.module.scss";
+import Spline from "@splinetool/react-spline";
+// import Link from "next/link";
+import Header from "../../components/Header";
 
 export default function Component() {
   return (
     <main className={styles.main}>
+      <Header />
 
-      <div className={styles.header}>
+      {/* <div className={styles.header}>
         <Link href="/about">
-        <div className={styles.logoContainer}>
-        <Image 
-        src={"/logoBlack.png"} width={90} height={91}
-        alt="logo"
-        className={styles.headerLogo}
-        />
-        {/* <h1 className={styles.logoName}>四條畷神社</h1> */}
-        </div>
-
+          <div className={styles.logoContainer}>
+            <Image
+              src={"/logoBlack.png"}
+              width={90}
+              height={91}
+              alt="logo"
+              className={styles.headerLogo}
+            />
+          </div>
         </Link>
-      </div>
-      
-  <div className={styles.loadingAnimation}>
-  <div className={styles.logoContainer}>
-  {/* <Image 
+      </div> */}
+
+      <div className={styles.loadingAnimation}>
+        <div className={styles.logoContainer}>
+          {/* <Image 
         src={"/logoBlack.png"} width={120} height={122}
         alt="logo"
         className={styles.symbol}
         /> */}
-        <iframe  width="180" height="120" 
-        src="https://rive.app/s/hXFbUXt7cUu1uzFVPps4Mw/embed" 
-        // allowfullscreen
-        className={styles.symbol} 
-        >
-        </iframe>
-        <h1>四條畷神社</h1>
+          <iframe
+            width="180"
+            height="120"
+            src="https://rive.app/s/hXFbUXt7cUu1uzFVPps4Mw/embed"
+            // allowfullscreen
+            className={styles.symbol}></iframe>
+          <h1>四條畷神社</h1>
         </div>
-  </div>
+      </div>
 
-<div className={styles.splineContainer}>
-  <Spline  scene="https://prod.spline.design/ga92tzKOHqSaXc1Q/scene.splinecode">
-  </Spline>
-</div>
-
+      <div className={styles.splineContainer}>
+        <Spline scene="https://prod.spline.design/ga92tzKOHqSaXc1Q/scene.splinecode"></Spline>
+      </div>
     </main>
-  )
+  );
 }
