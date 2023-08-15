dc1d624c53693de9c3578e5c51964d44219cf4da
soga-johnny <soga.johnny@gmail.com>
2023-08-15 16:46:24 +0900

    GADoubleDeclare

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -9,6 +9,8 @@ import { existsGaId, pageview } from "../src/app/lib/gtag";
 const GoogleAnalytics = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || "";
+
 
   useEffect(() => {
     if (!existsGaId) {
@@ -22,14 +24,14 @@ const GoogleAnalytics = () => {
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-SWVR8H6MYT`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
       <Script id="gtag-init" strategy="afterInteractive">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-SWVR8H6MYT', {
+            gtag('config', '${GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `}
