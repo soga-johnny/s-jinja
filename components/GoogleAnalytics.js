158815a9ed90d68821292d9dac141b9a04a77599
soga-johnny <soga.johnny@gmail.com>
2023-08-25 11:33:28 +0900

    addSuspense

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -3,13 +3,14 @@
 import { usePathname, useSearchParams } from "next/navigation";
 import Script from "next/script";
 // import { useRouter } from "next/router"
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { existsGaId, pageview } from "../lib/gtag";
+import { GA_TRACKING_ID } from "../lib/gtag";
 
-const GoogleAnalytics = () => {
+const Analytics = () => {
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || "";
+  // const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || "";
 
 
   useEffect(() => {
@@ -40,25 +41,17 @@ const GoogleAnalytics = () => {
   );
 };
 
-export default GoogleAnalytics;
 
-{
-  /* <Script
-strategy="afterInteractive"
-src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-/>
-<Script
-id="gtag-init"
-strategy="afterInteractive"
-dangerouslySetInnerHTML={{
-  __html: `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${gtag.GA_TRACKING_ID}', {
-      page_path: window.location.pathname,
-    });
-  `,
-}}
-/> */
+export default function GoogleAnalytics() {
+  return (
+    <>
+      {GA_TRACKING_ID && (
+        <Suspense>
+          <Analytics />
+        </Suspense>
+      )}
+    </>
+  );
 }
+
+// export default GoogleAnalytics;
