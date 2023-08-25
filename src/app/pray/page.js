0aa743bfe108e63582671146879b4cd9606dc856
soga-johnny <soga.johnny@gmail.com>
2023-08-25 16:19:04 +0900

    addToPRay

--- src/app/pray/page.js
+++ src/app/pray/page.js
@@ -1,5 +1,19 @@
 import PrayComponent from "./pray";
 
+const siteName = "境内のご案内";
+const url = "https://shijonawatejinja.or.jp/pray";
+
+export const metadata = {
+  title: siteName,
+  openGraph: {
+    title: siteName,
+    url,
+  },
+  twitter: {
+    title: siteName,
+  },
+};
+
 export default function Pray() {
   return <PrayComponent />;
 }
